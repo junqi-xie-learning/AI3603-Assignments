3b951197a8407d3ad159908ba0da7fe5783f5258
Junqi Xie <junqi_xie@outlook.com>
2021-09-29 14:36:35 +0800

    Implement A-star Algorithm

--- 1 Search Algorithm/A_star.py
+++ 1 Search Algorithm/A_star.py
@@ -1,11 +1,41 @@
 import DR20API
 import numpy as np
+import heapq
 
 ### START CODE HERE ###
 # This code block is optional. You can define your utility function and class in this block if necessary.
+def next_pos(current_map, current_pos):
+    """
+    Find all possible next positions of the current position.
 
+    Arguments:
+    current_map -- A 120*120 array indicating current map, where 0 indicating traversable and 1 indicating obstacles.
+    current_pos -- A 2D vector indicating the current position of the robot.
 
+    Return:
+    candidates -- A list of candidates of possible next positions
+    """
+    candidates = []
+    directions = np.array([[-1, 0], [1, 0], [0, -1], [0, 1]])
+    for direction in directions:
+        candidate = tuple(np.array(current_pos) + direction)
+        if current_map[candidate] == 0:
+            candidates.append(candidate)
+    return candidates
 
+def distance(current_pos, goal_pos):
+    """
+    Calculate the distance between current position and goal position.
+    Also used as the heuristic function for A* search.
+
+    Arguments:
+    current_pos -- A 2D vector indicating the current position of the robot.
+    goal_pos -- A 2D vector indicating the position of the goal.
+
+    Return:
+    distance -- An integer representing the distance between current position and goal position.
+    """
+    return np.linalg.norm(np.array(current_pos) - np.array(goal_pos))
 ###  END CODE HERE  ###
 
 def A_star(current_map, current_pos, goal_pos):
@@ -23,9 +53,25 @@ def A_star(current_map, current_pos, goal_pos):
     """
 
     ### START CODE HERE ###
+    current_pos = tuple(current_pos)
+    goal_pos = tuple(goal_pos)
 
-
-
+    fringe, closed = [], {}
+    backward, prev_pos = 0, None
+    while current_pos != goal_pos:
+        if current_pos not in closed:
+            closed[current_pos] = prev_pos
+            candidates = next_pos(current_map, current_pos)
+            for candidate in candidates:
+                priority = backward + 1 + distance(candidate, goal_pos)
+                heapq.heappush(fringe, (priority, candidate, current_pos, backward + 1))
+        _, current_pos, prev_pos, backward = heapq.heappop(fringe)
+    closed[current_pos] = prev_pos
+    
+    path = []
+    while current_pos:
+        path.insert(0, current_pos)
+        current_pos = closed[current_pos]
     ###  END CODE HERE  ###
     return path
 
@@ -43,9 +89,7 @@ def reach_goal(current_pos, goal_pos):
     """
 
     ### START CODE HERE ###
-
-
-
+    is_reached = distance(current_pos, goal_pos) <= 1
     ###  END CODE HERE  ###
     return is_reached
 
