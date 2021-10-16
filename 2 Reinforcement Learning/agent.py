f3b73e53b60b7f41248c5d2fb9a0242963efdedc
Junqi Xie <junqi_xie@outlook.com>
2021-10-16 15:08:46 +0800

    Implement Sarsa Agent

--- 2 Reinforcement Learning/agent.py
+++ 2 Reinforcement Learning/agent.py
@@ -17,27 +17,37 @@ You need to implement the Q-learning agent, Sarsa agent and Dyna-Q agent in this
 """TODO: Implement your Sarsa agent here"""
 class SarsaAgent(object):
     ##### START CODING HERE #####
-    def __init__(self, all_actions):
-        """initialize the agent. Maybe more function inputs are needed."""
+    def __init__(self, all_actions, alpha, gamma, epsilon):
+        """initialize the agent"""
         self.all_actions = all_actions
-        self.epsilon = 1.0
-    
+        self.q_values = {}
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+
     def choose_action(self, observation):
-        """choose action with epsilon-greedy algorithm."""
-        action = np.random.choice(self.all_actions)
+        """choose action with epsilon-greedy algorithm"""
+        if np.random.random() < self.epsilon:
+            action = np.random.choice(self.all_actions)
+        else:
+            q_values = [self.get_q(observation, action) for action in self.all_actions]
+            action = self.all_actions[q_values.index(max(q_values))]
         return action
     
-    def learn(self):
+    def learn(self, state, action, next_state, next_action, reward):
         """learn from experience"""
-        time.sleep(0.5)
-        print("[INFO] The learning process complete. (ﾉ｀⊿´)ﾉ")
-        return True
+        next_q = self.get_q(next_state, next_action)
+        self.learn_q(state, action, reward + self.gamma * next_q)
     
-    def your_function(self, params):
-        """You can add other functions as you wish."""
-        do_something = True
-        return None
-
+    def get_q(self, state, action):
+        """get Q-value from self.q_values"""
+        self.q_values.setdefault((state, action), 0)
+        return self.q_values[(state, action)]
+    
+    def learn_q(self, state, action, reward):
+        """update Q-value according to reward"""
+        self.q_values[(state, action)] = (1 - self.alpha) * self.get_q(state, action) + \
+            self.alpha * reward
     ##### END CODING HERE #####
 
 # ------------------------------------------------------------------------------------------- #
